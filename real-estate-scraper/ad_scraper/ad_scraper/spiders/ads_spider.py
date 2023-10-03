6fdcef35079d1805990941c4952426e462f03c38
Bendza <bendza@mail.com>
2023-10-03 18:41:17 +0200

    config and progress bar update

--- real-estate-scraper/ad_scraper/ad_scraper/spiders/ads_spider.py
+++ real-estate-scraper/ad_scraper/ad_scraper/spiders/ads_spider.py
@@ -3,17 +3,19 @@ from ad_scraper.website_configs import WEBSITE_CONFIGS
 
 class AdsSpider(scrapy.Spider):
     name = 'ads_spider'
-    total_pages = sum(start_url_config['max_pages'] for config in WEBSITE_CONFIGS.values() for start_url_config in config['start_urls'])
-    processed_pages = 0
+    total_requests = sum(start_url_config['max_pages'] for config in WEBSITE_CONFIGS.values() for start_url_config in config['start_urls'])
+    processed_requests = 0
+    total_items = 0 
 
     def start_requests(self):
-        for config in WEBSITE_CONFIGS.values():
+        for website, config in WEBSITE_CONFIGS.items():
+            self.current_website = website  # Set the current website being processed
             for start_url_config in config['start_urls']:
                 base_url = start_url_config['url']
                 pagination_pattern = config.get('pagination_pattern', '?page={page_number}')  # Default to '?page={page_number}' if not provided
-                for page in range(1, start_url_config['max_pages'] + 1):
-                    # Construct the URL with the pagination pattern
-                    url = f"{base_url}{pagination_pattern.format(page_number=page)}"
+                for page in range(0, start_url_config['max_pages']):
+                    offset = (page - 1) * 20
+                    url = f"{base_url}{pagination_pattern.format(page_number=page, offset=offset)}"
                     yield scrapy.Request(url, callback=self.parse, meta={'config': config})
 
     def parse(self, response):
@@ -25,8 +27,9 @@ class AdsSpider(scrapy.Spider):
                 'url': response.urljoin(link)  # This ensures the URL is absolute, not relative
             }
 
+        self.processed_requests += 1
+        self.total_items += len(ad_links)
+
         # Update progress
-        self.processed_pages += 1
-        progress_percentage = (self.processed_pages / self.total_pages) * 100
-        website_domain = response.url.split('/')[2]  # Extracts the domain from the URL
-        self.logger.info(f"Progress for {website_domain}: {progress_percentage:.2f}%")
+        progress_percentage = (self.processed_requests / self.total_requests) * 100
+        self.logger.info(f" Page: {self.processed_requests}/{self.total_requests} - Items:{self.total_items} ------------------------------------------- Complete: {progress_percentage:.2f}% /100%")
--- real-estate-scraper/ad_scraper/ad_scraper/website_configs.py
+++ real-estate-scraper/ad_scraper/ad_scraper/website_configs.py
@@ -1,19 +1,51 @@
 WEBSITE_CONFIGS = {
     'halooglasi.com': {
         'start_urls': [
-            {'url': 'https://www.halooglasi.com/nekretnine/prodaja-stanova/', 'max_pages': 800},
-            {'url': 'https://www.halooglasi.com/nekretnine/izdavanje-stanova/', 'max_pages': 250},
-            {'url': 'https://www.halooglasi.com/nekretnine/prodaja-kuca/', 'max_pages': 250},
-            {'url': 'https://www.halooglasi.com/nekretnine/izdavanje-kuca/', 'max_pages': 200}
+            {'url': 'https://www.halooglasi.com/nekretnine/prodaja-stanova/', 'max_pages': 780},
+            {'url': 'https://www.halooglasi.com/nekretnine/izdavanje-stanova/', 'max_pages': 220},
+            {'url': 'https://www.halooglasi.com/nekretnine/prodaja-kuca/', 'max_pages': 180},
+            {'url': 'https://www.halooglasi.com/nekretnine/izdavanje-kuca/', 'max_pages': 25}
         ],
         'ad_selector': 'h3.product-title a',
         'pagination_pattern': '?page={page_number}'
     },
     'nekretnine.rs': {
-        'start_urls':[
-            {'url': 'https://www.nekretnine.rs/stambeni-objekti/izdavanje-prodaja/lista/po-stranici/20/', 'max_pages': 200},
-        ],
+        'start_urls':[{'url': 'https://www.nekretnine.rs/stambeni-objekti/izdavanje-prodaja/lista/po-stranici/20/', 'max_pages':210}],
         'ad_selector': 'h2.offer-title a',
         'pagination_pattern': 'po-stranici/20/{page_number}/' 
-    }
+    },
+    'oglasi.rs': {
+        'start_urls': [{'url': 'https://www.oglasi.rs/nekretnine/prodaja-stanova', 'max_pages': 770},
+                       {'url': 'https://www.oglasi.rs/nekretnine/izdavanje-stanova', 'max_pages': 50},
+                       {'url': 'https://www.oglasi.rs/nekretnine/prodaja-kuca', 'max_pages': 160},
+                       {'url': 'https://www.oglasi.rs/nekretnine/izdavanje-kuca', 'max_pages': 10}],
+        'ad_selector': 'a.fpogl-list-title',
+        'pagination_pattern': '?i=96&p={page_number}'
+    },
+    'beogradskioglasi.com': {
+        'start_urls': [{'url': 'https://www.beogradskioglasi.com/oglasi/nekretnine/', 'max_pages': 130}],
+        'ad_selector': 'div.classified a',
+        'pagination_pattern': '?page={page_number}'
+    },
+    '4zida.rs': {
+        'start_urls': [{'url': 'https://www.4zida.rs/prodaja-stanova', 'max_pages': 101},
+                       {'url': 'https://www.4zida.rs/izdavanje-stanova', 'max_pages': 101},
+                       {'url': 'https://www.4zida.rs/prodaja-kuca', 'max_pages': 101},
+                       {'url': 'https://www.4zida.rs/izdavanje-kuca', 'max_pages': 52}],
+        'ad_selector': 'app-link a#internal',
+        'pagination_pattern': '?strana={page_number}'
+    },
+    'sasomange.rs': {
+        'start_urls': [{'url': 'https://sasomange.rs/c/stanovi-prodaja', 'max_pages': 870},
+                       {'url': 'https://sasomange.rs/c/stanovi-iznajmljivanje', 'max_pages': 200},
+                       {'url': 'https://sasomange.rs/c/kuce-prodaja', 'max_pages': 230},
+                       {'url': 'https://sasomange.rs/c/kuce-iznajmljivanje', 'max_pages': 20}],
+        'ad_selector': 'a.product-link',
+        'pagination_pattern': '?currentPage={page_number}'
+    },
+    'nadjidom.com': {
+        'start_urls': [{'url': 'https://www.nadjidom.com/sr/nekretnine', 'max_pages': 2260}],
+        'ad_selector': 'a.card-img-top',
+        'pagination_pattern': '&offset={offset}'
+    },
 }
